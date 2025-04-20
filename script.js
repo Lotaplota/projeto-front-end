4a3759d6613617cd96cb568abd77fb3acf24cd12
CayoMe <cayomendes@gmail.com>
2025-04-20 18:11:58 -0300

    Alteração do carrossel para funcionar como esperado

---
+++ script.js
@@ -0,0 +1,18 @@
+// Arquivo de JavaScript do projeto front-end
+
+// Demora um tempo considerável para explicar esse código, então duas opções: ou você lê o código e tenta entender, ou você pesquisa como ele funciona =) --Cayo.
+document.querySelectorAll('.slider-nav button').forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const idx = parseInt(btn.getAttribute('data-slide'));
+        const slider = document.querySelector('.slider-carrossel');
+        if (slider && slider.children.length > 0) {
+            const slideWidth = slider.children[0].clientWidth;
+            slider.scrollTo({
+                left: slideWidth * idx,
+                behavior: 'smooth'
+            });
+            document.querySelectorAll('.slider-nav button').forEach(b => b.classList.remove('active'));
+            btn.classList.add('active');
+        }
+    });
+});
